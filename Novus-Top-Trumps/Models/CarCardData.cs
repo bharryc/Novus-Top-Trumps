58d67d0a008c0202fa9cdf12b3f529e110dc07d8
Cyrus Bharry <bharryc@gmail.com>
2023-10-10 19:45:41 +0100

    Added auto population for the cars card as long as its empty

---
+++ Novus-Top-Trumps/Models/CarCardData.cs
@@ -0,0 +1,41 @@
+﻿namespace Novus_Top_Trumps.Models
+{
+    public class CarCardData
+    {
+        public static readonly List<CarsCards> CarDeck = new List<CarsCards>
+        {
+            new CarsCards{Name = "McLaren P1", Speed = 217, Horsepower = 903, Weight = 1390, Price = 866000},
+            new CarsCards{Name = "Ferrari LaFerrari", Speed = 217, Horsepower = 950, Weight = 1255, Price = 1000000},
+            new CarsCards{Name = "Porsche 918 Spyder", Speed = 214, Horsepower = 887, Weight = 1640, Price = 711000},
+            new CarsCards{Name = "Koenigsegg Regera", Speed = 249, Horsepower = 1500, Weight = 1470, Price = 1500000},
+            new CarsCards{Name = "Mercedes AMG Project One", Speed = 218, Horsepower = 1049, Weight = 1695, Price = 2400000},
+            new CarsCards{Name = "Aston Martin Valkyrie", Speed = 250, Horsepower = 1160, Weight = 1270, Price = 2500000},
+            new CarsCards{Name = "Pagani Huayra BC", Speed = 238, Horsepower = 791, Weight = 1250, Price = 2800000},
+            new CarsCards{Name = "Lamborghini Sian", Speed = 217, Horsepower = 808, Weight = 1594, Price = 2600000},
+            new CarsCards{Name = "Bugatti Chiron", Speed = 261, Horsepower = 1479, Weight = 1996, Price = 2500000},
+            new CarsCards{Name = "McLaren Senna", Speed = 211, Horsepower = 789, Weight = 1198, Price = 750000},
+            new CarsCards{Name = "Rimac Nevera", Speed = 258, Horsepower = 1914, Weight = 1950, Price = 1800000},
+            new CarsCards{Name = "McLaren SpeedTail", Speed = 250, Horsepower = 1036, Weight = 1499, Price = 2100000},
+            new CarsCards{Name = "McLaren F1", Speed = 240, Horsepower = 618, Weight = 1138, Price = 540000},
+            new CarsCards{Name = "Ferrari Enzo", Speed = 221, Horsepower = 651, Weight = 1365, Price = 450000},
+            new CarsCards{Name = "Lotus Evija", Speed = 218, Horsepower = 1972, Weight = 1678, Price = 2040000},
+            new CarsCards{Name = "Bugatti Veyron Super Sport", Speed = 267, Horsepower = 1200, Weight = 1838, Price = 1600000},
+            new CarsCards{Name = "Ford GT", Speed = 216, Horsepower = 660, Weight = 1385, Price = 420000},
+            new CarsCards{Name = "Lamborghini Veneno", Speed = 221, Horsepower = 740, Weight = 1450, Price = 3200000},
+            new CarsCards{Name = "Pagani Zonda Revolucion", Speed = 217, Horsepower = 789, Weight = 1070, Price = 2300000},
+            new CarsCards{Name = "Koenigsegg One:1", Speed = 273, Horsepower = 1340, Weight = 1360, Price = 2200000},
+            new CarsCards{Name = "Aston Martin Vulcan", Speed = 208, Horsepower = 820, Weight = 1350, Price = 1800000},
+            new CarsCards{Name = "Ferrari Daytona SP3", Speed = 211, Horsepower = 829, Weight = 1485, Price = 1700000},
+            new CarsCards{Name = "Hennessey Venom F5", Speed = 272, Horsepower = 1817, Weight = 1385, Price = 1500000},
+            new CarsCards{Name = "Zenvo TSR-S", Speed = 202, Horsepower = 1177, Weight = 1495, Price = 1560000},
+            new CarsCards{Name = "Ferrari F12 TDF", Speed = 211, Horsepower = 769, Weight = 1520, Price = 339000},
+            new CarsCards{Name = "McLaren 765LT", Speed = 205, Horsepower = 755, Weight = 1229, Price = 282000},
+            new CarsCards{Name = "Porsche GT2 RS", Speed = 211, Horsepower = 612, Weight = 1470, Price = 357000},
+            new CarsCards{Name = "Audi R8 GT", Speed = 199, Horsepower = 602, Weight = 1570, Price = 195000},
+            new CarsCards{Name = "Ferrari SF90 XX", Speed = 199, Horsepower = 1016, Weight = 1560, Price = 660000},
+            new CarsCards{Name = "Aston Martin Valhalla", Speed = 217, Horsepower = 937, Weight = 1550, Price = 700000},
+            new CarsCards{Name = "Koenigsegg Jesko", Speed = 295, Horsepower = 1603, Weight = 1320, Price = 2850000},
+            new CarsCards{Name = "Mercedes AMG GT Black Series", Speed = 202, Horsepower = 720, Weight = 1615, Price = 335000},
+        };
+    }
+}
