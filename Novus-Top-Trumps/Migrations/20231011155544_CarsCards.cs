33b34deb2f03c550e065a6ffc439142e6eed1f27
Joshua-Rex <joshua.porterlightwood@gmail.com>
2023-10-15 20:35:19 +0100

    Updated the migrations to allow for updating different decks and allowed for the init migration to create all existing tables.

--- Novus-Top-Trumps/Migrations/20231011155544_CarsCards.cs
+++ Novus-Top-Trumps/Migrations/20231011155544_CarsCards.cs
@@ -4,7 +4,7 @@
 
 namespace Novus_Top_Trumps.Migrations
 {
-    public partial class init : Migration
+    public partial class CarsCards : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -24,32 +24,12 @@ namespace Novus_Top_Trumps.Migrations
                {
                    table.PrimaryKey("PK_CarsCard", x => x.ID);
                });
-
-            migrationBuilder.CreateTable(
-                name: "PokemonCard",
-                columns: table => new
-                {
-                    ID = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Speed = table.Column<int>(type: "int", nullable: false),
-                    Attack = table.Column<int>(type: "int", nullable: false),
-                    Defence = table.Column<int>(type: "int", nullable: false),
-                    Health = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_PokemonCard", x => x.ID);
-                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
                 name: "CarsCard");
-
-            migrationBuilder.DropTable(
-                name: "PokemonCard");
         }
     }
 }
--- Novus-Top-Trumps/Migrations/20231011155544_PokemonCards.cs
+++ Novus-Top-Trumps/Migrations/20231011155544_PokemonCards.cs
@@ -4,26 +4,10 @@
 
 namespace Novus_Top_Trumps.Migrations
 {
-    public partial class init : Migration
+    public partial class PokemonCards : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-               name: "CarsCard",
-               columns: table => new
-               {
-                   ID = table.Column<int>(type: "int", nullable: false)
-                       .Annotation("SqlServer:Identity", "1, 1"),
-                   Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                   Speed = table.Column<int>(type: "int", nullable: false),
-                   Horsepower = table.Column<int>(type: "int", nullable: false),
-                   Weight = table.Column<int>(type: "int", nullable: false),
-                   Price = table.Column<int>(type: "int", nullable: false)
-               },
-               constraints: table =>
-               {
-                   table.PrimaryKey("PK_CarsCard", x => x.ID);
-               });
 
             migrationBuilder.CreateTable(
                 name: "PokemonCard",
@@ -45,9 +29,6 @@ namespace Novus_Top_Trumps.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "CarsCard");
-
             migrationBuilder.DropTable(
                 name: "PokemonCard");
         }
