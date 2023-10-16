d6f2d36b34c152a947a8ed22b5bbadcec3c330fa
Omar Rashad <omar-rashad@outlook.com>
2023-10-16 14:43:48 +0100

    Changed the button directory to no longer go to index but instead SelectAttribute

--- Novus-Top-Trumps/Controllers/DeckSelectorController.cs
+++ Novus-Top-Trumps/Controllers/DeckSelectorController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Novus_Top_Trumps.Data;
 using Novus_Top_Trumps.Models;
 
@@ -7,17 +8,20 @@ namespace Novus_Top_Trumps.Controllers
     public class DeckSelectorController : Controller
     {
 
+        [Authorize]
         public IActionResult DeckSelection()
         {
             return View();
         }
+
+        [Authorize]
         public IActionResult Index()
         {
             
             List<CustomItemModel> customItems = new List<CustomItemModel>
             {
-                new CustomItemModel { Link = "../CarsCards", Name = "Cars" },
-                new CustomItemModel { Link = "../PokemonCards", Name = "Pokemon" }
+                new CustomItemModel { Link = "../CarsCards/SelectAttribute", Name = "Cars" },
+                new CustomItemModel { Link = "../PokemonCards/SelectAttribute", Name = "Pokemon" }
             };
             
             ViewBag.CustomItems = customItems;
